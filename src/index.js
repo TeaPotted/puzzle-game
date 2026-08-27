134a0f59e51ab8110f1260b10d334721b3b4c2f6
Zoe Wong <146807323+TeaPotted@users.noreply.github.com>
2026-08-27 23:43:31 +0800

    create a function for checking if the given tile is allowed to swap with the empty div in .puzzleContainer

--- src/index.js
+++ src/index.js
@@ -78,4 +78,26 @@ function getTileIndex(tile) {
   }
 }
 
+// function for checking if the given tile is allowed to swap with the empty div
+function isSwapValid(tile) {
+  const [eX, eY] = getTileIndex(puzzleContainer.querySelector("#empty"));
+  const targetTileIndex = getTileIndex(tile);
+  // create an array of all adjacent tile indexes from div#empty
+  let possibleMoves = [
+    [eX - 1, eY], // left
+    [eX + 1, eY], // right
+    [eX, eY - 1], // down
+    [eX, eY + 1], // up
+  ];
+
+  // filter out the illegal moves (moves that are out of bounds)
+  possibleMoves = possibleMoves.filter((index) => {
+    if (index[0] >= 1 && index[0] <= 3 && index[1] >= 1 && index[1] <= 3)
+      return index;
+  });
+
+  // if targetTileIndex exists in possibleMoves then return true, else return false
+  return possibleMoves.some((a) => targetTileIndex.every((v, i) => v === a[i]));
+}
+
 displayShuffledImages();
