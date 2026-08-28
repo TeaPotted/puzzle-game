48aa9564e44eb9fac2cb50b16242f7c2fa4d1673
Zoe Wong <146807323+TeaPotted@users.noreply.github.com>
2026-08-29 01:34:41 +0800

    make images position start from 1 instead of 0

--- src/scripts/display.js
+++ src/scripts/display.js
@@ -13,15 +13,15 @@ import { swapTile } from "./tiles.js";
 const puzzleContainer = document.querySelector(".puzzle-container");
 // array to keep all the images
 const images = [
-  { pos: 0, elem: createImg(img0_0) },
-  { pos: 1, elem: createImg(img1_0) },
-  { pos: 2, elem: createImg(img2_0) },
-  { pos: 3, elem: createImg(img0_1) },
-  { pos: 4, elem: createImg(img1_1) },
-  { pos: 5, elem: createImg(img2_1) },
-  { pos: 6, elem: createImg(img0_2) },
-  { pos: 7, elem: createImg(img1_2) },
-  { pos: 8, elem: createImg(img2_2) },
+  { pos: 1, elem: createImg(img0_0) },
+  { pos: 2, elem: createImg(img1_0) },
+  { pos: 3, elem: createImg(img2_0) },
+  { pos: 4, elem: createImg(img0_1) },
+  { pos: 5, elem: createImg(img1_1) },
+  { pos: 6, elem: createImg(img2_1) },
+  { pos: 7, elem: createImg(img0_2) },
+  { pos: 8, elem: createImg(img1_2) },
+  { pos: 9, elem: createImg(img2_2) },
 ];
 
 // function that shuffles images array then appends each image to puzzleContainer
