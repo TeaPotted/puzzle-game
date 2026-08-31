7d8ea8c30ec6fde126c219f36ea1913f4c05980a
Zoe Wong <146807323+TeaPotted@users.noreply.github.com>
2026-08-31 23:04:13 +0800

    make functions that create div and h3 DOM elements

--- src/scripts/functions.js
+++ src/scripts/functions.js
@@ -5,13 +5,27 @@ function createImg(src) {
   return img;
 }
 
+// function for creating a div
+function createDiv(cls) {
+  const div = document.createElement("div");
+  div.classList.add(cls);
+  return div;
+}
+
+// function for creating a p element
+function createH3(cls, text) {
+  const h3 = document.createElement("h3");
+  h3.classList.add(cls);
+  h3.textContent = text;
+  return h3;
+}
+
 // shuffles the given array
 function shuffle(array) {
   for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * i + 1);
-    [array[i], array[j]] = [array[j], array[i]]
+    [array[i], array[j]] = [array[j], array[i]];
   }
 }
 
-
-export { createImg, shuffle };
+export { createImg, shuffle, createDiv, createH3 };
