dab7967da13bcd9a6d10e77c590ac8daefa7508e
Zoe Wong <146807323+TeaPotted@users.noreply.github.com>
2026-09-02 16:02:46 +0800

    create functions for when the player loses and wins

---
+++ src/scripts/game-endings.js
@@ -0,0 +1,33 @@
+import img2_2 from "../images/uhm-2-2.jpg";
+import { createImg, stopTimer } from "./functions.js";
+import { displayWinScreen, displayLoseScreen } from "./display.js";
+const puzzleContainer = document.querySelector(".puzzle-container");
+
+// function for when the player loses the game
+function loseGame() {
+  // remove all onlcick functions in puzzleContainer.children and call loseGame()
+  [...puzzleContainer.children].map((el) => (el.onclick = ""));
+  setTimeout(() => {
+    displayLoseScreen();
+  }, 800);
+}
+
+// function for when player solves the game
+function winGame() {
+  stopTimer();
+  // replace the empty div with the missing img
+  const lastImg = createImg(img2_2);
+  lastImg.id = "last-piece";
+
+  // remove onclick function on all children in puzzleContainer
+  [...puzzleContainer.children].map((el) => {
+    el.onclick = "";
+  });
+  puzzleContainer.querySelector("#empty").replaceWith(lastImg);
+
+  setTimeout(() => {
+    displayWinScreen();
+  }, 800);
+}
+
+export { winGame, loseGame };
