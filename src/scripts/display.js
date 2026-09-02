3ceab0fae876d2aea714e76efc47a7d2f721cc39
Zoe Wong <146807323+TeaPotted@users.noreply.github.com>
2026-09-02 17:20:16 +0800

    make replayBtn in displayWinScreen and displayLoseScreen call reset() when clicked

--- src/scripts/display.js
+++ src/scripts/display.js
@@ -15,6 +15,7 @@ import {
   createDiv,
   createH3,
   startTimer,
+  reset,
 } from "./functions.js";
 import { swapTile } from "./tiles.js";
 
@@ -106,6 +107,7 @@ function displayWinScreen() {
   const winDiv = createDiv("win-div");
   const h3 = createH3("win", "YOU WIN!");
   const replayBtn = createImg(replayImg);
+  replayBtn.onclick = () => reset();
   winDiv.append(h3, replayBtn);
   document.body.append(winDiv);
 }
@@ -145,6 +147,7 @@ function displayLoseScreen() {
   const loseDiv = createDiv("lose-div");
   const h3 = createH3("lose", "YOU LOSE!");
   const restartBtn = createImg(replayImg);
+  restartBtn.onclick = () => reset()
   loseDiv.append(h3, restartBtn);
   document.body.append(loseDiv);
 }
